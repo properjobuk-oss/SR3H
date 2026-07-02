f405bb557a1a9ad3ed432e772299344612cce9f2
Danny Griffin <dannygriffin@yahoo.com>
2026-07-02 16:34:40 +0100

    Use SR3H blue for browser chrome

--- site.js
+++ site.js
@@ -42,34 +42,4 @@ if (document.documentElement.classList.contains("intro-enabled")) {
   introOverlay?.addEventListener("click", closeIntro);
 }
 
-const themeColor = document.querySelector("#theme-color");
-const themedSections = [...document.querySelectorAll("[data-theme-color]")];
-
-if (themeColor && themedSections.length) {
-  let ticking = false;
-
-  const syncThemeColor = () => {
-    const marker = window.scrollY + window.innerHeight * 0.28;
-    let active = themedSections[0];
-
-    themedSections.forEach((section) => {
-      if (section.offsetTop <= marker) {
-        active = section;
-      }
-    });
-
-    themeColor.setAttribute("content", active.dataset.themeColor);
-    ticking = false;
-  };
-
-  const queueThemeColor = () => {
-    if (!ticking) {
-      window.requestAnimationFrame(syncThemeColor);
-      ticking = true;
-    }
-  };
-
-  syncThemeColor();
-  window.addEventListener("scroll", queueThemeColor, { passive: true });
-  window.addEventListener("resize", queueThemeColor);
-}
+document.querySelector("#theme-color")?.setAttribute("content", "#2e6fd8");
